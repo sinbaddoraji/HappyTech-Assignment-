d2672d2c2c072fe39193ea97ecfa13ffffeee4b4
nikitadalela2001 <nikita.dalela2001@gmail.com>
2020-11-05 17:25:38 +0000

    classes and attributes

---
+++ Happy Tech Gui Main/Happy Tech Gui Main/Happy Tech Gui Main/TemplateModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Happy_Tech_Gui_Main
+{
+    public class TemplateModel
+    {
+        public int template_ID { get; set; }
+        public int template_Header_ID { get; set; }
+        public int template_Body_ID { get; set; }
+    }
+}
---
+++ Happy Tech Gui Main/Happy Tech Gui Main/Happy Tech Gui Main/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Happy_Tech_Gui_Main
+{
+    public class User
+    {
+        public int user_ID { get; set; }
+        public string userFirstName { get; set; }
+        public string userSurname { get; set; }
+        public string userPassword { get; set; }
+    }
+}
+    
