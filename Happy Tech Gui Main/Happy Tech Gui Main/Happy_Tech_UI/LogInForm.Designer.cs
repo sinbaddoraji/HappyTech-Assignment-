9a152d1da85fb14ae80892d07b54305c49368a02
Escuder-Dev <israelaguilarg96@gmail>
2020-11-06 13:09:38 +0000

    Update Loging Button:

--- Happy Tech Gui Main/Happy Tech Gui Main/Happy_Tech_UI/LogInForm.Designer.cs
+++ Happy Tech Gui Main/Happy Tech Gui Main/Happy_Tech_UI/LogInForm.Designer.cs
@@ -42,26 +42,30 @@
             // textBox1
             // 
             this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.textBox1.Location = new System.Drawing.Point(119, 59);
+            this.textBox1.Location = new System.Drawing.Point(89, 48);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(160, 22);
+            this.textBox1.Size = new System.Drawing.Size(120, 20);
             this.textBox1.TabIndex = 0;
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(119, 97);
+            this.textBox2.Location = new System.Drawing.Point(89, 79);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(2);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(160, 22);
+            this.textBox2.Size = new System.Drawing.Size(121, 20);
             this.textBox2.TabIndex = 1;
+            this.textBox2.TextChanged += new System.EventHandler(this.TextBox2_TextChanged);
             // 
             // button1
             // 
             this.button1.BackColor = System.Drawing.SystemColors.ButtonShadow;
             this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.button1.Location = new System.Drawing.Point(300, 59);
+            this.button1.Location = new System.Drawing.Point(225, 48);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(81, 60);
+            this.button1.Size = new System.Drawing.Size(61, 49);
             this.button1.TabIndex = 2;
             this.button1.Text = "LOG IN";
             this.button1.UseVisualStyleBackColor = false;
@@ -72,9 +76,10 @@
             this.label1.AutoSize = true;
             this.label1.BackColor = System.Drawing.SystemColors.ActiveBorder;
             this.label1.ForeColor = System.Drawing.Color.Black;
-            this.label1.Location = new System.Drawing.Point(23, 64);
+            this.label1.Location = new System.Drawing.Point(17, 52);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(75, 17);
+            this.label1.Size = new System.Drawing.Size(56, 13);
             this.label1.TabIndex = 4;
             this.label1.Text = "username:";
             this.label1.Click += new System.EventHandler(this.label1_Click);
@@ -84,18 +89,20 @@
             this.label2.AutoSize = true;
             this.label2.BackColor = System.Drawing.SystemColors.ActiveBorder;
             this.label2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.label2.Location = new System.Drawing.Point(23, 97);
+            this.label2.Location = new System.Drawing.Point(17, 79);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(72, 17);
+            this.label2.Size = new System.Drawing.Size(55, 13);
             this.label2.TabIndex = 5;
             this.label2.Text = "password:";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(325, 106);
+            this.label3.Location = new System.Drawing.Point(244, 86);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(153, 17);
+            this.label3.Size = new System.Drawing.Size(120, 13);
             this.label3.TabIndex = 6;
             this.label3.Text = "SIGN IN TO ACCOUNT";
             this.label3.Click += new System.EventHandler(this.label3_Click);
@@ -110,18 +117,20 @@
             this.panel1.Controls.Add(this.textBox2);
             this.panel1.Controls.Add(this.label2);
             this.panel1.ForeColor = System.Drawing.Color.Snow;
-            this.panel1.Location = new System.Drawing.Point(189, 147);
+            this.panel1.Location = new System.Drawing.Point(142, 119);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(416, 185);
+            this.panel1.Size = new System.Drawing.Size(312, 151);
             this.panel1.TabIndex = 7;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(302, 68);
+            this.label4.Location = new System.Drawing.Point(226, 55);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(194, 38);
+            this.label4.Size = new System.Drawing.Size(163, 31);
             this.label4.TabIndex = 24;
             this.label4.Text = "HappyTech";
             this.label4.TextAlign = System.Drawing.ContentAlignment.TopCenter;
@@ -129,16 +138,18 @@
             // 
             // LogInForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ButtonShadow;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(600, 366);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.panel1);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "LogInForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.LogInForm_Load);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
             this.ResumeLayout(false);
--- Happy Tech Gui Main/Happy Tech Gui Main/Happy_Tech_UI/LogInForm.cs
+++ Happy Tech Gui Main/Happy Tech Gui Main/Happy_Tech_UI/LogInForm.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,7 @@ namespace Happy_Tech_UI
 {
     public partial class LogInForm : Form
     {
+
         public LogInForm()
         {
             InitializeComponent();
@@ -24,7 +26,23 @@ namespace Happy_Tech_UI
 
         private void button1_Click(object sender, EventArgs e)
         {
-           
+
+            {
+                SqlConnection con = new SqlConnection(@"Data Source=USER; Initial Catalog=admin; Integrated Security=True"); // making connection   
+                SqlDataAdapter sda = new SqlDataAdapter("SELECT COUNT(*) FROM login WHERE username='" + textBox1.Text + "' AND password='" + textBox2.Text + "'", con);
+                /* in above line the program is selecting the whole data from table and the matching it with the user name and password provided by user. */
+                DataTable dt = new DataTable(); //this is creating a virtual table  
+                sda.Fill(dt);
+                if (dt.Rows[0][0].ToString() == "1")
+                {
+                    /* I have made a new page called home page. If the user is successfully authenticated then the form will be moved to the next form */
+                    this.Hide();
+                    new FeedbackForm().Show();
+                }
+                else
+                    MessageBox.Show("Invalid username or password");
+            }
+            
         }
 
         private void label3_Click(object sender, EventArgs e)
@@ -38,6 +56,16 @@ namespace Happy_Tech_UI
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void LogInForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TextBox2_TextChanged(object sender, EventArgs e)
         {
 
         }
