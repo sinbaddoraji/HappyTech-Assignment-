eb77430865c13f3024cf11b1ac8cdd12b40da6f3
Amneziiaa <71885829+Amneziiaa@users.noreply.github.com>
2020-11-06 14:01:39 +0000

    Checkbox coding

--- Happy Tech Gui Main/Happy Tech Gui Main/Happy_Tech_UI/FeedbackForm.cs
+++ Happy Tech Gui Main/Happy Tech Gui Main/Happy_Tech_UI/FeedbackForm.cs
@@ -21,6 +21,13 @@ namespace Happy_Tech_UI
         {
 
         }
+        public void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked == true)
+            {
+                textBox1.Text = " Successful";
+            }
+        }
 
         public void checkBox1_CheckedChanged(object sender, EventArgs e)
         {
@@ -28,12 +35,6 @@ namespace Happy_Tech_UI
 
         }
 
-        public void textBox1_TextChanged(object sender, EventArgs e)
-        {
-            if(checkBox1.Checked == true)
-            {
-                textBox1.Text = " Suc";
-            }
-        }
+       
     }
 }
